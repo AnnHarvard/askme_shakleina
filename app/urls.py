503dba9562d98ae8ac242da2067d50d79f2d07f2
AnnHarvard <dana.shakleina@yandex.ru>
2024-05-22 01:19:06 +0300

    continue create templates

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('hot/', views.hot, name='index')
+    path('hot/', views.hot, name='index'),
+    path('questions/<int:question_id>', views.question, name='question'),
 ]
-
--- app/views.py
+++ app/views.py
@@ -1,19 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
 QUESTIONS = [
     {
         "title": f"Question {i}",
         "text": f"This is question number {i}"
-    } for i in range(10)
+    } for i in range(200)
 ]
 
 
 def index(request):
-    return render(request, 'index.html', {"questions": QUESTIONS})
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(QUESTIONS, 5)
+    page_obj = paginator.page(page_num)
+    return render(request, 'index.html', {"questions": page_obj})
 
 
 def hot(request):
     questions = QUESTIONS[5:]
     return render(request, 'hot.html', {"questions": questions})
+
+
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, 'question_detail.html', {"question": item})

d39c7b445bd97dc921832948bf88d00b07c1ad71
AnnHarvard <dana.shakleina@yandex.ru>
2024-05-26 06:51:51 +0300

    started dz3

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    tags = models.ManyToManyField(Tag)
